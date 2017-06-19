11d081967e39294f93704d283088a555e142e478
soo-park <sparkstacks@gmail.com>
2017-06-19 11:02:47 -0700

    body parser added

--- server/index.js
+++ server/index.js
@@ -27,3 +27,28 @@ app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
 
+
+
+// // instantiate a new Clarifai app passing in your clientId and clientSecret
+// var app = new Clarifai.App(
+//   'FVTbO2wMgN1ovmKN2td8PRPz-lT_rv3p7XCGmqTf',
+//   '7xSrYMq2KsDhuZ966gUH0A3ItfjBeN4MgWNLpLCw'
+// );
+
+// // predict the contents of an image by passing in a url
+// app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+//   function(response) {
+//     console.log(response);
+//   },
+//   function(err) {
+//     console.error(err);
+//   }
+// );
+
+// // response in clarifai.sample.json
+
+
+
+
+
+
