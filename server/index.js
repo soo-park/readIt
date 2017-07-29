0e7694adb6c64231cb6abc3aaefd6f16312ac739
soo-park <sparkstacks@gmail.com>
2017-07-28 23:40:56 -0700

    port added for deployment

--- server/index.js
+++ server/index.js
@@ -74,8 +74,9 @@ app.post('/items/search', function (req, res) {
 
 })
 
+var port = process.env.PORT || 3001;
 
-app.listen(3001, function() {
+app.listen(port, function() {
   console.log('listening on port 3001!');
 });
 
